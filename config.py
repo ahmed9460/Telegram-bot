96bafe2ebb11c2442b160f500d8ddfd406df931a
ahmed9460 <57077004+ahmed9460@users.noreply.github.com>
2022-02-06 13:09:52 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5030986245:AAGdeSu1hCLSMdyLatTRMf3OyTAQJFyiKWI"
+BOT_TOKEN = "5218758519:AAEBxKe1Totvp5c0kF3v4ZYT-_ay884RJ5E"
 APP_URL = "https://capahmedbot.herokuapp.com/" + BOT_TOKEN
